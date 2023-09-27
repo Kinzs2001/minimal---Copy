6a67cfa51790778640c48779e709660f634c05f4
Piyush Saxena <saxenapiyush876@gmail.com>
2023-09-27 10:27:30 +0530

    first commit

---
+++ src/Routes/allRoutes.js
@@ -0,0 +1,172 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+
+// Base Ui
+import UiAlerts from "../pages/BaseUi/UiAlerts/UiAlerts";
+import UiBadges from "../pages/BaseUi/UiBadges/UiBadges";
+import UiButtons from "../pages/BaseUi/UiButtons/UiButtons";
+import UiColors from "../pages/BaseUi/UiColors/UiColors";
+import UiCards from "../pages/BaseUi/UiCards/UiCards";
+import UiCarousel from "../pages/BaseUi/UiCarousel/UiCarousel";
+import UiDropdowns from "../pages/BaseUi/UiDropdowns/UiDropdowns";
+import UiGrid from "../pages/BaseUi/UiGrid/UiGrid";
+import UiImages from "../pages/BaseUi/UiImages/UiImages";
+import UiTabs from "../pages/BaseUi/UiTabs/UiTabs";
+import UiAccordions from "../pages/BaseUi/UiAccordion&Collapse/UiAccordion&Collapse";
+import UiModals from "../pages/BaseUi/UiModals/UiModals";
+import UiOffcanvas from "../pages/BaseUi/UiOffcanvas/UiOffcanvas";
+import UiPlaceholders from "../pages/BaseUi/UiPlaceholders/UiPlaceholders";
+import UiProgress from "../pages/BaseUi/UiProgress/UiProgress";
+import UiNotifications from "../pages/BaseUi/UiNotifications/UiNotifications";
+import UiMediaobject from "../pages/BaseUi/UiMediaobject/UiMediaobject";
+import UiEmbedVideo from "../pages/BaseUi/UiEmbedVideo/UiEmbedVideo";
+import UiTypography from "../pages/BaseUi/UiTypography/UiTypography";
+import UiList from "../pages/BaseUi/UiLists/UiLists";
+import UiGeneral from "../pages/BaseUi/UiGeneral/UiGeneral";
+import UiRibbons from "../pages/BaseUi/UiRibbons/UiRibbons";
+import UiUtilities from "../pages/BaseUi/UiUtilities/UiUtilities";
+
+// Advance Ui
+import UiNestableList from "../pages/AdvanceUi/UiNestableList/UiNestableList";
+import UiScrollbar from "../pages/AdvanceUi/UiScrollbar/UiScrollbar";
+import UiAnimation from "../pages/AdvanceUi/UiAnimation/UiAnimation";
+import UiTour from "../pages/AdvanceUi/UiTour/UiTour";
+import UiSwiperSlider from "../pages/AdvanceUi/UiSwiperSlider/UiSwiperSlider";
+import UiRatings from "../pages/AdvanceUi/UiRatings/UiRatings";
+import UiHighlight from "../pages/AdvanceUi/UiHighlight/UiHighlight";
+
+
+
+//AuthenticationInner pages
+import BasicSignIn from '../pages/AuthenticationInner/Login/BasicSignIn';
+import CoverSignIn from '../pages/AuthenticationInner/Login/CoverSignIn';
+import BasicSignUp from '../pages/AuthenticationInner/Register/BasicSignUp';
+import CoverSignUp from "../pages/AuthenticationInner/Register/CoverSignUp";
+import BasicPasswReset from '../pages/AuthenticationInner/PasswordReset/BasicPasswReset';
+//pagesmport Gallery from '../pages/Pages/Gallery/Gallery';
+
+import CoverPasswReset from '../pages/AuthenticationInner/PasswordReset/CoverPasswReset';
+import BasicLockScreen from '../pages/AuthenticationInner/LockScreen/BasicLockScr';
+import CoverLockScreen from '../pages/AuthenticationInner/LockScreen/CoverLockScr';
+import BasicLogout from '../pages/AuthenticationInner/Logout/BasicLogout';
+import CoverLogout from '../pages/AuthenticationInner/Logout/CoverLogout';
+import BasicSuccessMsg from '../pages/AuthenticationInner/SuccessMessage/BasicSuccessMsg';
+import CoverSuccessMsg from '../pages/AuthenticationInner/SuccessMessage/CoverSuccessMsg';
+import BasicTwosVerify from '../pages/AuthenticationInner/TwoStepVerification/BasicTwosVerify';
+import CoverTwosVerify from '../pages/AuthenticationInner/TwoStepVerification/CoverTwosVerify';
+import Basic404 from '../pages/AuthenticationInner/Errors/Basic404';
+import Cover404 from '../pages/AuthenticationInner/Errors/Cover404';
+import Alt404 from '../pages/AuthenticationInner/Errors/Alt404';
+import Error500 from '../pages/AuthenticationInner/Errors/Error500';
+
+import BasicPasswCreate from "../pages/AuthenticationInner/PasswordCreate/BasicPasswCreate";
+import CoverPasswCreate from "../pages/AuthenticationInner/PasswordCreate/CoverPasswCreate";
+import Offlinepage from "../pages/AuthenticationInner/Errors/Offlinepage";
+
+//login
+import Login from "../pages/Authentication/Login";
+import ForgetPasswordPage from "../pages/Authentication/ForgetPassword";
+import Logout from "../pages/Authentication/Logout";
+import Register from "../pages/Authentication/Register";
+
+
+// User Profile
+import UserProfile from "../pages/Authentication/user-profile";
+
+//APi Key
+import APIKey from "../pages/APIKey/index";
+import UiLinks from "../pages/BaseUi/UiLinks/UiLinks";
+
+const authProtectedRoutes = [
+ 
+
+  // Base Ui
+  { path: "/ui-alerts", component: <UiAlerts /> },
+  { path: "/ui-badges", component: <UiBadges /> },
+  { path: "/ui-buttons", component: <UiButtons /> },
+  { path: "/ui-colors", component: <UiColors /> },
+  { path: "/ui-links", component: <UiLinks /> },
+  { path: "/ui-cards", component: <UiCards /> },
+  { path: "/ui-carousel", component: <UiCarousel /> },
+  { path: "/ui-dropdowns", component: <UiDropdowns /> },
+  { path: "/ui-grid", component: <UiGrid /> },
+  { path: "/ui-images", component: <UiImages /> },
+  { path: "/ui-tabs", component: <UiTabs /> },
+  { path: "/ui-accordions", component: <UiAccordions /> },
+  { path: "/ui-modals", component: <UiModals /> },
+  { path: "/ui-offcanvas", component: <UiOffcanvas /> },
+  { path: "/ui-placeholders", component: <UiPlaceholders /> },
+  { path: "/ui-progress", component: <UiProgress /> },
+  { path: "/ui-notifications", component: <UiNotifications /> },
+  { path: "/ui-media", component: <UiMediaobject /> },
+  { path: "/ui-embed-video", component: <UiEmbedVideo /> },
+  { path: "/ui-typography", component: <UiTypography /> },
+  { path: "/ui-lists", component: <UiList /> },
+  { path: "/ui-general", component: <UiGeneral /> },
+  { path: "/ui-ribbons", component: <UiRibbons /> },
+  { path: "/ui-utilities", component: <UiUtilities /> },
+
+  // Advance Ui
+  { path: "/advance-ui-nestable", component: <UiNestableList /> },
+  { path: "/advance-ui-scrollbar", component: <UiScrollbar /> },
+  { path: "/advance-ui-animation", component: <UiAnimation /> },
+  { path: "/advance-ui-tour", component: <UiTour /> },
+  { path: "/advance-ui-swiper", component: <UiSwiperSlider /> },
+  { path: "/advance-ui-ratings", component: <UiRatings /> },
+  { path: "/advance-ui-highlight", component: <UiHighlight /> },
+
+ 
+
+  
+  //Api Key
+  { path: "/apps-api-key", component: <APIKey /> },
+
+  //User Profile
+  { path: "/profile", component: <UserProfile /> },
+
+  // this route should be at the end of all other routes
+  // eslint-disable-next-line react/display-name
+  {
+    path: "/",
+    exact: true,
+    component: <Navigate to="/dashboard" />,
+  },
+  { path: "*", component: <Navigate to="/dashboard" /> },
+];
+
+const publicRoutes = [
+  // Authentication Page
+  { path: "/logout", component: <Logout /> },
+  { path: "/login", component: <Login /> },
+  { path: "/forgot-password", component: <ForgetPasswordPage /> },
+  { path: "/register", component: <Register /> },
+
+  //AuthenticationInner pages
+  { path: "/auth-signin-basic", component: <BasicSignIn /> },
+  { path: "/auth-signin-cover", component: <CoverSignIn /> },
+  { path: "/auth-signup-basic", component: <BasicSignUp /> },
+  { path: "/auth-signup-cover", component: <CoverSignUp /> },
+  { path: "/auth-pass-reset-basic", component: <BasicPasswReset /> },
+  { path: "/auth-pass-reset-cover", component: <CoverPasswReset /> },
+  { path: "/auth-lockscreen-basic", component: <BasicLockScreen /> },
+  { path: "/auth-lockscreen-cover", component: <CoverLockScreen /> },
+  { path: "/auth-logout-basic", component: <BasicLogout /> },
+  { path: "/auth-logout-cover", component: <CoverLogout /> },
+  { path: "/auth-success-msg-basic", component: <BasicSuccessMsg /> },
+  { path: "/auth-success-msg-cover", component: <CoverSuccessMsg /> },
+  { path: "/auth-twostep-basic", component: <BasicTwosVerify /> },
+  { path: "/auth-twostep-cover", component: <CoverTwosVerify /> },
+  { path: "/auth-404-basic", component: <Basic404 /> },
+  { path: "/auth-404-cover", component: <Cover404 /> },
+  { path: "/auth-404-alt", component: <Alt404 /> },
+  { path: "/auth-500", component: <Error500 /> },
+
+  
+  { path: "/auth-pass-change-basic", component: <BasicPasswCreate /> },
+  { path: "/auth-pass-change-cover", component: <CoverPasswCreate /> },
+  { path: "/auth-offline", component: <Offlinepage /> },
+
+];
+
+export { authProtectedRoutes, publicRoutes };
---
+++ src/store/actions.js
@@ -0,0 +1,14 @@
+// Layout
+export * from "./layouts/action";
+// Authentication module
+export * from "./auth/register/actions";
+export * from "./auth/login/actions";
+export * from "./auth/forgetpwd/actions";
+export * from "./auth/profile/actions";
+
+// File Manager
+export * from "./fileManager/action"
+
+//API Key
+export * from "./apikey/action";
+
