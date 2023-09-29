b7b98878af524cf4ad75f253e6251dd45d7c0e63
Piyush Saxena <saxenapiyush876@gmail.com>
2023-09-29 18:18:10 +0530

    .

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
 import VerticalLayout from "../Layouts/index";
-
+import Login from "../pages/Authentication/Login";
+import Logout from "../pages/Authentication/Logout";
+import Register from "../pages/Authentication/Register";
+import User from "../pages/Authentication/user-profile";
 
 
 const Index = () => {
     return (
         <React.Fragment>
             <Routes>
-                <Route path="/dashboard" element={<VerticalLayout/>} />
+                {/* <Route path="/login" element={<Login/>} /> */}
+                <Route path="/" element={<VerticalLayout/>} />
+                {/* <Route path="/logout" element={<Logout/>} />
+                <Route path="/register" element={<Register/>} />
+                <Route path="/user" element={<User/>} /> */}
             </Routes>
         </React.Fragment>
     );
