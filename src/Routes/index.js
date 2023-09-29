04a986452d75661cd11b3f99d70882fc6eeb1619
Piyush Saxena <saxenapiyush876@gmail.com>
2023-09-29 17:49:59 +0530

    .

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,47 +1,14 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
-//Layouts
-// import NonAuthLayout from "../Layouts/NonAuthLayout";
 import VerticalLayout from "../Layouts/index";
 
-//routes
-// import { authProtectedRoutes, publicRoutes } from "./allRoutes";
-// import { AuthProtected } from './AuthProtected';
+
 
 const Index = () => {
     return (
         <React.Fragment>
             <Routes>
-                {/* <Route>
-                    {publicRoutes.map((route, idx) => (
-                        <Route
-                            path={route.path}
-                            element={
-                                <>
-                                    {route.component}
-                                </>
-                            }
-                            key={idx}
-                            exact={true}
-                        />
-                    ))}
-                </Route> 
-
-                <Route>
-                    {authProtectedRoutes.map((route, idx) => (
-                        <Route
-                            path={route.path}
-                            element={
-                                <AuthProtected>
-                                    <VerticalLayout>{route.component}</VerticalLayout>
-                                </AuthProtected>
-                                }
-                            key={idx}
-                            exact={true}
-                        />
-                    ))}
-                </Route> */}
                 <Route path="/dashboard" element={<VerticalLayout/>} />
             </Routes>
         </React.Fragment>
